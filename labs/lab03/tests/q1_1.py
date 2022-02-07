e86d796fe27b247d9a18c743305fedbdaad33812
Adam Poliak <azpoliak@gmail.com>
2022-02-07 17:39:51 -0500

    lab03

---
+++ labs/lab03/tests/q1_1.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q1_1',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> # Make sure your function has the proper syntax!;\n>>> to_percentage(.35) == 35.0\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> # Make sure your function has the proper syntax!;\n>>> to_percentage(.925) == 92.5\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
