e6c6a65f9f43107b76e4f19cfa02c2761b672f4c
Adam Poliak <azpoliak@gmail.com>
2022-03-03 10:23:25 -0500

    lab05

---
+++ labs/lab05/tests/q8_2.py
@@ -0,0 +1,10 @@
+test = {   'name': 'q8_2',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> len(simulated_statistics) == 1000\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.all(simulated_statistics <= 30)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.all(simulated_statistics >= 0)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> 0 <= simulation_and_statistic(model_proportions, expected_proportion_correct)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
